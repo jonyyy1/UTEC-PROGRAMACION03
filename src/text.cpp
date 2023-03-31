89a10a3fef6f9277fd581b849ddb3bd00b3d8113
jonyyy1 <jonathan.quilca@utec.edu.pe>
2023-03-31 16:36:00 -0500

    FIRST COMMIT

--- src/text.cpp
+++ src/text.cpp
@@ -7,3 +7,23 @@
 using namespace std;
 
 
+Text::Text(const char *_text) {
+
+}
+
+Text::Text(const Text &other) {
+
+}
+
+Text::Text(Text &&other) {
+
+}
+
+
+Text Text::operator*(const Text &other) {
+    return Text();
+}
+
+Text Text::operator*=(const Text &other) {
+    return Text();
+}
--- src/text.h
+++ src/text.h
@@ -4,5 +4,35 @@
 
 #ifndef PROG3_UNIT1_TEXT_V2023_1_TEXT_H
 #define PROG3_UNIT1_TEXT_V2023_1_TEXT_H
+//class atributos privados por defecto
+//struct atributos publicos por defecto
+#include "iostream"
+using namespace std;
+class Text{
+    char* _text = nullptr; //inicializacion in class
+    int _sz = 0;
+public:
+    Text() = default; //constructor por defecto
+    Text(const char* _text); //constructor por parametros
+    Text(const Text& other); //constructor copia
+    Text(Text&& other); //constructor move
+
+    Text& operator=(const Text& other); //Asignacion copy
+    Text& operator=(Text&& other);
+    //sobrecarga
+    Text operator*(const Text& other);
+    Text operator*=(const Text& other);
+
+    friend ostream& operator<<(ostream& out, const Text& text);
+    friend istream& operator>>(istream& out, const Text& text);
+
+
+};
+
+ostream& operator<<(ostream& out, const Text& text);
+istream& operator>>(istream& out, const Text& text);
+
+//Text& operator=(Text& text, const string& other);
+//string& operator=(const string&, const Text& other);
 
 #endif //PROG3_UNIT1_TEXT_V2023_1_TEXT_H
